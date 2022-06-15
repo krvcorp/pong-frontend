f669ca3b9c34b36e7ada7d71f5dac0c2f72c201c
Khoi Nguyen <khoinguyen@Khois-MBP.fios-router.home>
2022-06-14 22:30:04 -0400

    info.plist force light mode

--- pong_frontend/pong_frontend/PongApp.swift
+++ pong_frontend/pong_frontend/PongApp.swift
@@ -1,14 +1,15 @@
 //
-//  SidechatMockupApp.swift
-//  SidechatMockup
+//  PongMockupApp.swift
+//  PongMockup
 //
 //  Created by Khoi Nguyen on 6/3/22.
 //
+// info plist Product Name used to be $(TARGET_NAME)
 
 import SwiftUI
 
 @main
-struct SidechatMockupApp: App {
+struct Pong: App {
     
     var body: some Scene {
         WindowGroup {
