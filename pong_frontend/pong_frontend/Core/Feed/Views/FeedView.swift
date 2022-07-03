4ea9c36f7717652cb6b5ed7285180dc7af198454
Khoi Nguyen <khoinguyen@Khois-MBP.fios-router.home>
2022-07-02 21:18:33 -0400

    update logo for pull to refresh

--- pong_frontend/pong_frontend/Core/Feed/Views/FeedView.swift
+++ pong_frontend/pong_frontend/Core/Feed/Views/FeedView.swift
@@ -126,4 +126,41 @@ struct FeedView: View {
     }
 }
 
+// investigate distance to drag
+struct PullToRefresh: View {
+    
+    var coordinateSpaceName: String
+    var onRefresh: ()->Void
+    
+    @State var needRefresh: Bool = false
+    
+    var body: some View {
+        GeometryReader { geo in
+            if (geo.frame(in: .named(coordinateSpaceName)).maxY > 200) {
+                Spacer()
+                    .onAppear {
+                        needRefresh = true
+                    }
+            } else if (geo.frame(in: .named(coordinateSpaceName)).maxY < 150) {
+                Spacer()
+                    .onAppear {
+                        if needRefresh {
+                            needRefresh = false
+                            onRefresh()
+                        }
+                    }
+            }
+            HStack {
+                Spacer()
+                if needRefresh {
+                    ProgressView()
+                } else {
+                    Image(systemName: "arrow.down")
+                }
+                Spacer()
+            }
+        }.padding(.top, -50)
+    }
+}
+
 
--- pong_frontend/pong_frontend/Extensions.swift
+++ pong_frontend/pong_frontend/Extensions.swift
@@ -35,38 +35,4 @@ extension UINavigationController: UIGestureRecognizerDelegate {
     }
 }
 
-struct PullToRefresh: View {
-    
-    var coordinateSpaceName: String
-    var onRefresh: ()->Void
-    
-    @State var needRefresh: Bool = false
-    
-    var body: some View {
-        GeometryReader { geo in
-            if (geo.frame(in: .named(coordinateSpaceName)).maxY > 250) {
-                Spacer()
-                    .onAppear {
-                        needRefresh = true
-                    }
-            } else if (geo.frame(in: .named(coordinateSpaceName)).maxY < 150) {
-                Spacer()
-                    .onAppear {
-                        if needRefresh {
-                            needRefresh = false
-                            onRefresh()
-                        }
-                    }
-            }
-            HStack {
-                Spacer()
-                if needRefresh {
-                    ProgressView()
-                } else {
-                    Text("⬇️")
-                }
-                Spacer()
-            }
-        }.padding(.top, -50)
-    }
-}
+
