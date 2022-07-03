6e6c2cdf6d4caad7e51d45a2d6d6e047216de7c0
Raunak <raunak@salesbricks.com>
2022-07-03 18:02:37 -0400

    Not req

--- backend/base/models.py
+++ backend/base/models.py
@@ -72,7 +72,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = "email"
     EMAIL_FIELD = "email"
-    REQUIRED_FIELDS = ["phone"]
+    REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = "User"
