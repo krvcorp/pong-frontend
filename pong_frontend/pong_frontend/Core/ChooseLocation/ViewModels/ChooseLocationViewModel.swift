59ef51e8b1ae41a9847b1858678eb0ea2c554389
Khoi Nguyen <khoinguyen@Khois-MBP.fios-router.home>
2022-07-02 23:55:35 -0400

    choose location page

--- pong_frontend/pong_frontend/Core/ChooseLocation/ViewModels/ChooseLocationViewModel.swift
+++ pong_frontend/pong_frontend/Core/ChooseLocation/ViewModels/ChooseLocationViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+enum ChooseLocationViewModel: Int, CaseIterable {
+    case harvard
+    case mit
+    case uva
+    case vt
+    
+    var title: String {
+        switch self {
+        case .harvard: return "Harvard"
+        case .mit: return "MIT"
+        case .uva: return "UVA"
+        case .vt: return "VT"
+        }
+    }
+}
--- pong_frontend/pong_frontend/Core/ChooseLocation/Views/ChooseLocationView.swift
+++ pong_frontend/pong_frontend/Core/ChooseLocation/Views/ChooseLocationView.swift
@@ -12,6 +12,7 @@ struct ChooseLocationView: View {
     @State private var selectedFilter: ChooseLocationFilterViewModel = .all
     @Namespace var animation
     @State private var text = ""
+    var columns = Array(repeating: GridItem(.fixed(UIScreen.main.bounds.size.width/2)), count: 2)
     
     
     var body: some View {
@@ -85,7 +86,36 @@ struct ChooseLocationView: View {
     }
     
     var gridView: some View {
-        Text("Yuh")
+        TabView(selection: $selectedFilter) {
+            ForEach(ChooseLocationFilterViewModel.allCases, id: \.self) { view in
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 30) {
+                        ForEach(ChooseLocationViewModel.allCases, id: \.rawValue) { viewModel in
+                            Button {
+                                print("DEBUG: Choose \(viewModel.title)")
+                            } label: {
+                                VStack {
+                                    VStack {
+                                        Text("\(viewModel.title)")
+                                        }
+                                    }
+                                }
+                                .frame(minWidth: 0, maxWidth: UIScreen.main.bounds.size.width/3, minHeight: UIScreen.main.bounds.size.width/5)
+                                .font(.system(size: 18).bold())
+                                .padding()
+                                .foregroundColor(Color(UIColor.label))
+                                .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color(UIColor.secondaryLabel), lineWidth: 10))
+                            }
+                            // remove highlight on tap
+                            .background(Color(UIColor.systemBackground)) // If you have this. change to background of school?
+                            .cornerRadius(20)         // You also need the cornerRadius here
+                    }
+                }.tag(view.rawValue) // by having the tag be the enum's raw value,
+                // you can always compare enum to enum.
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        .ignoresSafeArea(.all, edges: .bottom)
     }
 }
 
