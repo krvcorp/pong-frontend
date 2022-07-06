1607ca7bc4677ae40a0dbd6e6e13035c899ab7bb
Khoi Nguyen <khoinguyen@Khois-MacBook-Pro.local>
2022-07-06 00:59:19 -0400

    added google oauth early

--- pong_frontend/Pods/Target Support Files/Pods-Pong/Pods-Pong-frameworks.sh
+++ pong_frontend/Pods/Target Support Files/Pods-Pong/Pods-Pong-frameworks.sh
@@ -179,11 +179,15 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AppAuth/AppAuth.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMAppAuth/GTMAppAuth.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleSignIn/GoogleSignIn.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleSignInSwiftSupport/GoogleSignInSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AppAuth/AppAuth.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMAppAuth/GTMAppAuth.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleSignIn/GoogleSignIn.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleSignInSwiftSupport/GoogleSignInSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- pong_frontend/pong_frontend/Core/Authentication/Views/OnboardView.swift
+++ pong_frontend/pong_frontend/Core/Authentication/Views/OnboardView.swift
@@ -5,13 +5,19 @@
 //  Created by Khoi Nguyen on 6/3/22.
 //
 
+import UIKit
 import SwiftUI
+import GoogleSignInSwift
+import GoogleSignIn
+
+let signInConfig = GIDConfiguration(clientID: "983201170682-kttqq1l89i4fpgk15fud1u1hf192fq1q.apps.googleusercontent.com")
 
 struct OnboardView: View {
     @Binding var email: String
     @Binding var password: String
     @Environment(\.presentationMode) var presentationMode
     @ObservedObject var loginVM : LoginViewModel
+    
     
     var body: some View {
 
@@ -49,11 +55,76 @@ struct OnboardView: View {
                 }
                 .background(Color(UIColor.label)) // If you have this
                 .cornerRadius(20)         // You also need the cornerRadius here
+                
+                Button {
+                    handleSignInButton()
+                } label: {
+                    Text("Awake")
+                }
+
+//                GoogleSignInButton(action: handleSignInButton)
             }
         }
         .padding(20)
     }
 }
+
+func handleSignInButton() {
+    guard let presentingViewController = UIApplication.shared.windows.first?.rootViewController else {
+      print("There is no root view controller!")
+      return
+    }
+    
+      GIDSignIn.sharedInstance.signIn(
+        with: signInConfig,
+        presenting: presentingViewController) { user, error in
+          guard let signInUser = user else {
+            // Inspect error
+            return
+          }
+          // If sign in succeeded, display the app's main content View.
+        }
+}
+
+
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(
+      _ app: UIApplication,
+      open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+      var handled: Bool
+
+      handled = GIDSignIn.sharedInstance.handle(url)
+      if handled {
+        return true
+      }
+
+      // Handle other custom URL types.
+
+      // If not handled by this app, return false.
+      return false
+    }
+    
+    func application(
+      _ application: UIApplication,
+      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+      GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+        if error != nil || user == nil {
+          // Show the app's signed-out state.
+        } else {
+          // Show the app's signed-in state.
+        }
+      }
+      return true
+    }
+
+
+}
+
+
 
 //struct OnboardView_Previews: PreviewProvider {
 //    static var previews: some View {
