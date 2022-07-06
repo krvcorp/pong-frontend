42b5abf589ebe4422700b7aaa34dca4b2450cf03
Khoi Nguyen <khoinguyen@Khois-MacBook-Pro.local>
2022-07-06 00:11:34 -0400

    post delete cuserdata

---
+++ pong_frontend/Pong/PongApp.swift
@@ -0,0 +1,17 @@
+//
+//  PongApp.swift
+//  Pong
+//
+//  Created by Khoi Nguyen on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
