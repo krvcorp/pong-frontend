cf8e47c1a9805ea491fd86b48ae64db9cd70da97
Raunak <raunak@salesbricks.com>
2022-07-22 22:37:30 -0400

    New structs

---
+++ pong_frontend/pong_frontend/Models/DeleteCommentRequestBody.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct DeleteCommentRequestBody: Encodable {
+    let comment_id: String
+}
--- pong_frontend/pong_frontend/Models/LoggedInInfoResponseBody.swift
+++ pong_frontend/pong_frontend/Models/LoggedInInfoResponseBody.swift
@@ -11,7 +11,7 @@ import Foundation
 //
 struct LoggedInUserInfoResponseBody: Decodable {
     let id: String
-    let email: String
+    let email: String?
     let posts: [Post]
     let comments: [Comment]
     let inTimeout: Bool
