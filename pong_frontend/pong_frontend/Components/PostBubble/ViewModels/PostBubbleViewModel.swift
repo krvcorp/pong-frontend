9d4b9add7f8ef4281021da903c962455a73b3d8e
Khoi Nguyen <khoinguyen12312000@gmail.com>
2022-07-24 18:02:41 -0400

    #172 added delete post function on frontend

--- pong_frontend/pong_frontend/Components/PostBubble/ViewModels/PostBubbleViewModel.swift
+++ pong_frontend/pong_frontend/Components/PostBubble/ViewModels/PostBubbleViewModel.swift
@@ -89,4 +89,29 @@ class PostBubbleViewModel: ObservableObject {
             
         }.resume()
     }
+    
+    func deletePost(postId: String, completion: @escaping (Result<String, AuthenticationError>) -> Void) {
+        guard let token = DAKeychain.shared["token"] else { return }
+        guard let url = URL(string: "\(API().root)post/") else {
+            completion(.failure(.custom(errorMessage: "URL is not correct")))
+            return
+        }
+        
+        let body = CreatePostReportRequestBody(postId: postId)
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Token \(token)", forHTTPHeaderField: "Authorization")
+        let encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        request.httpBody = try? encoder.encode(body)
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard let data = data, error == nil else {
+                completion(.failure(.custom(errorMessage: "No data")))
+                return
+            }
+        }.resume()
+    }
 }
