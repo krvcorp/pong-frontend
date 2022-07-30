074cf74216ad0615ff261937ba13357aa3ed8204
Raunak Daga <raunakdaga@gmail.com>
2022-07-30 17:38:15 -0400

    Removed backend

--- backend/base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from base import views
-from rest_framework.authtoken.views import obtain_auth_token
-
-urlpatterns = [
-    # General URLS
-    path("", views.index, name="index"),
-]
