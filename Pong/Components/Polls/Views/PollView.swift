5b81c7058398c3a211e5813a06a17d07d6bc64e8
Khoi Nguyen <khoinguyen12312000@gmail.com>
2022-07-30 21:47:57 -0400

    renamed postsettings folder and prepping for poll component

---
+++ Pong/Components/Polls/Views/PollView.swift
@@ -0,0 +1,20 @@
+//
+//  PollView.swift
+//  Pong
+//
+//  Created by Khoi Nguyen on 7/30/22.
+//
+
+import SwiftUI
+
+struct PollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PollView_Previews: PreviewProvider {
+    static var previews: some View {
+        PollView()
+    }
+}
