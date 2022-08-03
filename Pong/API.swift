502da613650b1517f59f42df869a10b01f883dae
Raunak Daga <raunakdaga@gmail.com>
2022-08-03 01:16:56 -0400

    Removed API extra

--- Pong/API.swift
+++ Pong/API.swift
@@ -13,40 +13,7 @@ enum AuthenticationError: Error {
     case custom(errorMessage: String)
 }
 
-enum NetworkError: Error {
-    case invalidURL
-    case noData
-    case decodingError
-}
-
 class API: ObservableObject {
     var root: String = "http://localhost:8005/api/"
     var rootAuth: String = "http://localhost:8005/auth/"
-    @Published var posts: [Post] = []
-    
-    func getPosts() {
-        
-        guard let token = DAKeychain.shared["token"] else { return } // Fetch
-        
-        guard let url = URL(string: "\(root)" + "post/") else { return }
-
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.addValue("Token \(token)", forHTTPHeaderField: "Authorization")
-        
-        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
-            
-            guard let data = data, error == nil else { return }
-            
-            do {
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                let posts = try decoder.decode([Post].self, from: data)
-                DispatchQueue.main.async {
-                    self?.posts = posts
-                }
-            } catch { }
-        }
-        task.resume()
-    }
 }
