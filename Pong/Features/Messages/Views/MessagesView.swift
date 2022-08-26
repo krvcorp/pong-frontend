59656d8aece0a00b326dd3c1d5749520bd64e9a4
Artemas Radik <ajradik@gmail.com>
2022-08-26 13:24:17 -0400

    Revert "Revert "Notifs""
    This reverts commit 4eb7578f5d750024599f45e93d67e293b235abd9.

--- Pong/Features/Messages/Views/MessagesView.swift
+++ Pong/Features/Messages/Views/MessagesView.swift
@@ -1,74 +1,164 @@
+// MIT License
+//
+// Copyright (c) 2017-2020 MessageKit
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import InputBarAccessoryView
+import MessageKit
+
 import SwiftUI
 
-struct MessagesView: View {
-    
-    @State private var searchText = ""
-    @State private var showAlert = false
-    
-    struct ChatModel: Identifiable {
-        var id: String { title }
-        let title: String
-        let subtitle: String
-        let timestamp: String
-        let new: Bool
-        let color1: Color
-        let color2: Color
-    }
-    
-    let chatmodels: [ChatModel] = [ChatModel(title: "Brattle street when jefes moves in", subtitle: "Why would you post that", timestamp: "6:09 PM", new: true, color1: Color.releaseNotesGradient1, color2: Color.releaseNotesGradient2), ChatModel(title: "I am getting housing at adams, but I'm also secretly", subtitle: "How much for the appt", timestamp: "4:20 PM", new: true, color1: Color.analyticsRevenueGradient1, color2: Color.analyticsRevenueGradient2), ChatModel(title: "Winthrop dining hall vibes are fire", subtitle: "No they're not", timestamp: "3:07 PM", new: false, color1: Color.webListItemGradient1, color2: Color.blue), ChatModel(title: "What if I 👉👈 got the HSA bigger bed", subtitle: "What if you didn't", timestamp: "2:59 PM", new: false, color1: Color.trackingLinksGradient1, color2: Color.trackingLinksGradient2), ChatModel(title: "is 1011a THAT hard...", subtitle: "Yes", timestamp: "1:06 PM", new: false, color1: Color.guestListGradient1, color2: Color.guestListGradient2)]
-    
-    var body: some View {
-        LoadingView(isShowing: .constant(false)) {
-            List {
-                Section(header: Text("Messages")) {
-                    ForEach(chatmodels.filter { searchText.isEmpty || $0.title.localizedStandardContains(searchText)}) { chatmodel in
-                        NavigationLink(destination: Text("messaging feed here")) {
-                            HStack {
-                                LinearGradient(gradient: Gradient(colors: [chatmodel.color1, chatmodel.color2]), startPoint: .bottomLeading, endPoint: .topTrailing).clipShape(Circle()).frame(width: 40, height: 40, alignment: .center)
-                                    .padding(.trailing, 4)
-                                VStack (alignment: .leading, spacing: 6) {
-                                    Text(chatmodel.title).bold().lineLimit(1)
-                                    HStack {
-                                        Text(chatmodel.subtitle).lineLimit(1).foregroundColor(.gray)
-                                        Spacer()
-                                        ZStack {
-                                            if chatmodel.new {
-                                                LinearGradient(gradient: Gradient(colors: [Color.viewEventsGradient1, Color.viewEventsGradient2]), startPoint: .bottomLeading, endPoint: .topTrailing)
-                                            } else {
-                                                Color(UIColor.secondarySystemFill)
-                                            }
-                                            Text(chatmodel.timestamp).foregroundColor(chatmodel.new ? .white : .gray).bold().lineLimit(1)
-                                        }
-                                        .cornerRadius(6)
-                                        .frame(width: 75)
-                                    }
-                                }
-                            }.padding(.vertical, 10)
-                        }
-                    }
-                }
-            }
-            .listStyle(GroupedListStyle())
-            .onAppear {
-                UITableView.appearance().showsVerticalScrollIndicator = false
-            }
-            .navigationTitle("Messages")
-//            .navigationBarTitleDisplayMode(.inline)
-            .searchable(text: $searchText)
-        }
-    }
-    
-    func dontEnableNotifs() {
-        
-    }
-    
-    func enableNotifs() {
-        
-    }
+// MARK: - MessageSwiftUIVC
+
+final class MessageSwiftUIVC: MessagesViewController {
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    // Because SwiftUI wont automatically make our controller the first responder, we need to do it on viewDidAppear
+    becomeFirstResponder()
+    messagesCollectionView.scrollToLastItem(animated: true)
+  }
 }
 
-struct MessagesView_Previews: PreviewProvider {
-    static var previews: some View {
-        MessagesView()
+// MARK: - MessagesView
+
+struct MessagesView: UIViewControllerRepresentable {
+  // MARK: Internal
+
+  final class Coordinator {
+    // MARK: Lifecycle
+
+    init(messages: Binding<[MessageType]>) {
+      self.messages = messages
     }
+
+    // MARK: Internal
+
+    let formatter: DateFormatter = {
+      let formatter = DateFormatter()
+      formatter.dateStyle = .medium
+      return formatter
+    }()
+
+    var messages: Binding<[MessageType]>
+  }
+
+  @State var initialized = false
+  @Binding var messages: [MessageType]
+
+  func makeUIViewController(context: Context) -> MessagesViewController {
+    let messagesVC = MessageSwiftUIVC()
+
+    messagesVC.messagesCollectionView.messagesDisplayDelegate = context.coordinator
+    messagesVC.messagesCollectionView.messagesLayoutDelegate = context.coordinator
+    messagesVC.messagesCollectionView.messagesDataSource = context.coordinator
+    messagesVC.messageInputBar.delegate = context.coordinator
+    messagesVC.scrollsToLastItemOnKeyboardBeginsEditing = true // default false
+    messagesVC.maintainPositionOnInputBarHeightChanged = true // default false
+    messagesVC.showMessageTimestampOnSwipeLeft = true // default false
+
+    return messagesVC
+  }
+
+  func updateUIViewController(_ uiViewController: MessagesViewController, context _: Context) {
+    uiViewController.messagesCollectionView.reloadData()
+    scrollToBottom(uiViewController)
+  }
+
+  func makeCoordinator() -> Coordinator {
+    Coordinator(messages: $messages)
+  }
+
+  // MARK: Private
+
+  private func scrollToBottom(_ uiViewController: MessagesViewController) {
+    DispatchQueue.main.async {
+      // The initialized state variable allows us to start at the bottom with the initial messages without seeing the initial scroll flash by
+      uiViewController.messagesCollectionView.scrollToLastItem(animated: self.initialized)
+      self.initialized = true
+    }
+  }
+}
+
+// MARK: - MessagesView.Coordinator + MessagesDataSource
+
+extension MessagesView.Coordinator: MessagesDataSource {
+  var currentSender: SenderType {
+    SampleData.shared.currentSender
+  }
+
+  func messageForItem(at indexPath: IndexPath, in _: MessagesCollectionView) -> MessageType {
+    messages.wrappedValue[indexPath.section]
+  }
+
+  func numberOfSections(in _: MessagesCollectionView) -> Int {
+    messages.wrappedValue.count
+  }
+
+  func messageTopLabelAttributedText(for message: MessageType, at _: IndexPath) -> NSAttributedString? {
+    let name = message.sender.displayName
+    return NSAttributedString(
+      string: name,
+      attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption1)])
+  }
+
+  func messageBottomLabelAttributedText(for message: MessageType, at _: IndexPath) -> NSAttributedString? {
+    let dateString = formatter.string(from: message.sentDate)
+    return NSAttributedString(
+      string: dateString,
+      attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption2)])
+  }
+
+  func messageTimestampLabelAttributedText(for message: MessageType, at _: IndexPath) -> NSAttributedString? {
+    let sentDate = message.sentDate
+    let sentDateString = MessageKitDateFormatter.shared.string(from: sentDate)
+    let timeLabelFont: UIFont = .boldSystemFont(ofSize: 10)
+    let timeLabelColor: UIColor = .systemGray
+    return NSAttributedString(
+      string: sentDateString,
+      attributes: [NSAttributedString.Key.font: timeLabelFont, NSAttributedString.Key.foregroundColor: timeLabelColor])
+  }
+}
+
+// MARK: - MessagesView.Coordinator + InputBarAccessoryViewDelegate
+
+extension MessagesView.Coordinator: InputBarAccessoryViewDelegate {
+  func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+    let message = MockMessage(text: text, user: SampleData.shared.currentSender, messageId: UUID().uuidString, date: Date())
+    messages.wrappedValue.append(message)
+    inputBar.inputTextView.text = ""
+  }
+}
+
+// MARK: - MessagesView.Coordinator + MessagesLayoutDelegate, MessagesDisplayDelegate
+
+extension MessagesView.Coordinator: MessagesLayoutDelegate, MessagesDisplayDelegate {
+  func configureAvatarView(_ avatarView: AvatarView, for message: MessageType, at _: IndexPath, in _: MessagesCollectionView) {
+    let avatar = SampleData.shared.getAvatarFor(sender: message.sender)
+    avatarView.set(avatar: avatar)
+  }
+
+  func messageTopLabelHeight(for _: MessageType, at _: IndexPath, in _: MessagesCollectionView) -> CGFloat {
+    20
+  }
+
+  func messageBottomLabelHeight(for _: MessageType, at _: IndexPath, in _: MessagesCollectionView) -> CGFloat {
+    16
+  }
 }
