c7f06af698a372603ef7fde5a74a63abfedaa817
Khoi Nguyen <khoinguyen12312000@gmail.com>
2022-08-26 12:20:15 -0400

    close #148, added set tabbar to scroll to top of list

---
+++ Pong/Features/MainTab/ViewModels/ScrollToTopHelper.swift
@@ -0,0 +1,15 @@
+//
+//  ScrollToTopHelper.swift
+//  Pong
+//
+//  Created by Khoi Nguyen on 8/26/22.
+//
+
+import Foundation
+
+class ScrollToTopHelper : ObservableObject {
+    @Published var trigger : Bool = false
+    
+    
+    
+}
