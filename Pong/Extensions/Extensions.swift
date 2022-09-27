a1f19b24169110e59032d8e37882e0d40d47daa5
Khoi Nguyen <khoinguyen@college.harvard.edu>
2022-09-26 20:07:30 -0400

    Revert "Merge branch 'main' into marketplace"
    This reverts commit be08ef1062bc47c4d9b3c9185f8967364706a6e3, reversing
    changes made to 82db6f90825a5b4e3f2a2b2454c4f800d5d1efe3.

--- Pong/Extensions/Extensions.swift
+++ Pong/Extensions/Extensions.swift
@@ -84,20 +84,3 @@ struct NavigationLazyView<Content: View>: View {
     }
 }
 
-
-extension View {
-  func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
-    background(
-      GeometryReader { geometryProxy in
-        Color.clear
-          .preference(key: SizePreferenceKey.self, value: geometryProxy.size)
-      }
-    )
-    .onPreferenceChange(SizePreferenceKey.self, perform: onChange)
-  }
-}
-
-private struct SizePreferenceKey: PreferenceKey {
-  static var defaultValue: CGSize = .zero
-  static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
-}
