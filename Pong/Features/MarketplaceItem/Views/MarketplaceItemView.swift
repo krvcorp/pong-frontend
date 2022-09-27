ed199e091d3ac81569f7996f64a3f86b21cc3601
Khoi Nguyen <khoinguyen@college.harvard.edu>
2022-09-26 20:37:44 -0400

    Revert "Merge pull request #293 from pongsocial/revert-292-marketplace"
    This reverts commit 7424487b4586f78483195e6ad6a185db8025557f, reversing
    changes made to 8504d93f7d61e3290480174f63095c9804e8c1ce.

---
+++ Pong/Features/MarketplaceItem/Views/MarketplaceItemView.swift
@@ -0,0 +1,107 @@
+//
+//  MarketplaceItemView.swift
+//  Pong
+//
+//  Created by Khoi Nguyen on 9/25/22.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct MarketplaceItemView: View {
+    @Binding var marketplaceItem: MarketplaceItem
+    
+    var body: some View {
+        ScrollView {
+            VStack{
+                // MARK: Image
+                if let imageUrl = marketplaceItem.image {
+                    KFImage(URL(string: "\(imageUrl)")!)
+                        .resizable()
+                        .scaledToFit()
+                        .aspectRatio(contentMode: .fill)
+                        .layoutPriority(-1)
+                        .cornerRadius(10)
+                }
+                
+                VStack {
+                    HStack {
+                        Text("\(marketplaceItem.title)")
+                            .font(.title2.bold())
+                        
+                        Spacer()
+                    }
+                    HStack {
+                        Text("\(marketplaceItem.price)")
+                            .font(.title.bold())
+                            .foregroundColor(SchoolManager.shared.schoolPrimaryColor())
+                        
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        Text("Posted on \(marketplaceItem.date)")
+                            .font(.subheadline)
+                        Spacer()
+                    }
+                }
+                
+                Button {
+                    print("DEBUG: reserve")
+                } label: {
+                    HStack {
+                        Image(systemName: "cart")
+                        
+                        Text("Reserve")
+                            .font(.title.bold())
+                    }
+                    .padding()
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .background(SchoolManager.shared.schoolPrimaryColor())
+                    .cornerRadius(10)
+
+                }
+
+                Button {
+                    print("DEBUG: message")
+                } label: {
+                    HStack {
+                        Image(systemName: "bubble.left")
+                        
+                        Text("Message")
+                            .font(.title.bold())
+                    }
+                    .padding()
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .background(Color(UIColor.systemBackground))
+                    .foregroundColor(Color(UIColor.label))
+                    .cornerRadius(10)
+
+                }
+                
+                Button {
+                    print("DEBUG: report")
+                } label: {
+                    HStack {
+                        Text("Report")
+                            .font(.title2)
+                    }
+                    .padding()
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .background(Color(UIColor.systemBackground))
+                    .foregroundColor(Color(UIColor.lightGray))
+                    .cornerRadius(10)
+
+                }
+            }
+            .padding()
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("\(marketplaceItem.title)")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "share")
+                }
+            }
+        }
+    }
+}
---
+++ Pong/Features/NewMarketplaceItem/Views/NewMarketplaceItem.swift
@@ -0,0 +1,140 @@
+//
+//  NewMarketplaceItem.swift
+//  Pong
+//
+//  Created by Khoi Nguyen on 9/25/22.
+//
+
+import SwiftUI
+
+struct NewMarketplaceItem: View {
+    @Environment(\.presentationMode) var presentationMode
+    @StateObject var newMarketplaceItemVM = NewMarketplaceItemViewModel()
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                // MARK: 'NavigationBar'
+                HStack(alignment: .center) {
+                    Button {
+                        self.presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Image(systemName: "xmark")
+                            .foregroundColor(Color(UIColor.label))
+                    }
+                    .padding()
+                    
+                    Text("New Item")
+                        .font(.title.bold())
+                    
+                    Spacer()
+                    
+                    Button {
+                        print("DEBUG: Create")
+                    } label: {
+                        Text("Create")
+                            .frame(minWidth: 100, maxWidth: 150)
+                            .font(.system(size: 18).bold())
+                            .padding()
+                            .foregroundColor(Color(UIColor.systemBackground))
+                            .overlay(RoundedRectangle(cornerRadius: 10).stroke(SchoolManager.shared.schoolPrimaryColor(), lineWidth: 2)
+                        )
+                    }
+                    .background(SchoolManager.shared.schoolPrimaryColor()) // If you have this
+                    .cornerRadius(10)         // You also need the cornerRadius here
+                }
+                
+                // MARK: Add media
+                HStack {
+                    Text("Add photos")
+                        .foregroundColor(Color(UIColor.lightGray))
+                    Spacer()
+                }
+                
+                HStack {
+                    Button {
+                        print("DEBUG: Camera")
+                    } label: {
+                        HStack {
+                            HStack(spacing: 5) {
+                                Spacer()
+                                Image(systemName: "camera")
+                                    .foregroundColor(SchoolManager.shared.schoolPrimaryColor())
+                                Text("Camera")
+                                    .font(.headline.bold())
+                                    .foregroundColor(Color(UIColor.label))
+                                Spacer()
+                            }
+                            .padding()
+
+                        }
+                        .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color(UIColor.systemGray), lineWidth: 2))
+                        .padding(2)
+
+                    }
+                    
+                    Button {
+                        print("DEBUG: Gallery")
+                    } label: {
+                        HStack {
+                            HStack(spacing: 5) {
+                                Spacer()
+                                Image(systemName: "photo")
+                                    .foregroundColor(SchoolManager.shared.schoolPrimaryColor())
+                                Text("Gallery")
+                                    .font(.headline.bold())
+                                    .foregroundColor(Color(UIColor.label))
+                                Spacer()
+                            }
+                            .padding()
+                        }
+                        .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color(UIColor.systemGray), lineWidth: 2))
+                        .padding(2)
+                    }
+                }
+                .padding(.bottom)
+                
+                // MARK: Add post information
+                HStack {
+                    Text("Fill in the details")
+                        .foregroundColor(Color(UIColor.lightGray))
+                    Spacer()
+                }
+                VStack {
+                    TextArea("Title *", text: $newMarketplaceItemVM.title)
+                        .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color(UIColor.systemGray), lineWidth: 2))
+                        .padding(2)
+                    
+                    HStack {
+                        TextArea("Price *", text: $newMarketplaceItemVM.price)
+                            .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color(UIColor.systemGray), lineWidth: 2))
+                            .padding(2)
+                        
+                        TextArea("Size", text: $newMarketplaceItemVM.size)
+                            .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color(UIColor.systemGray), lineWidth: 2))
+                            .padding(2)
+                    }
+                    
+                    TextArea("Brand", text: $newMarketplaceItemVM.brand)
+                        .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color(UIColor.systemGray), lineWidth: 2))
+                        .padding(2)
+                    
+                    TextArea("Description", text: $newMarketplaceItemVM.description)
+                        .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color(UIColor.systemGray), lineWidth: 2))
+                        .padding(2)
+                    
+                }
+                
+                Spacer()
+                
+            }
+            .padding()
+        }
+    }
+}
+
+struct NewMarketplaceItem_Previews: PreviewProvider {
+    static var previews: some View {
+        NewMarketplaceItem()
+    }
+}
