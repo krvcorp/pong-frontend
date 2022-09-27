47ab7491093c7141cc39cf846fcdbb60b09edb33
Khoi Nguyen <khoinguyen@college.harvard.edu>
2022-09-26 20:10:49 -0400

    deleted useless comments

--- Pong/Features/CommunityMenu/Views/ChooseCommunityView.swift
+++ Pong/Features/CommunityMenu/Views/ChooseCommunityView.swift
@@ -8,25 +8,6 @@
 import SwiftUI
 
 struct ChooseCommunityView: View {
-    //    var body: some View {
-    //        VStack(alignment: .leading) {
-    //            Text("Check out these communities!")
-    //                .bold()
-    //                .padding(.top)
-    //                .padding(.horizontal)
-    //
-    //            List {
-    //                Text("All colleges!")
-    //                Text("Boston University")
-    //            }
-    //            .refreshable {
-    //                print("DEBUG: Community Refresh")
-    //            }
-    //            .background(Color(UIColor.secondarySystemBackground))
-    //        }
-    //        .background(Color(UIColor.secondarySystemBackground))
-    //        .frame(maxWidth: .infinity, alignment: .leading)
-    //    }
     var body: some View {
         VStack(alignment: .leading) {
             Text("Visit other communities!")
