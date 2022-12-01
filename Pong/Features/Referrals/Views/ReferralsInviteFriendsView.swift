73410434a0fb59f2b4b34cd7d0f36a5f4f03e2ca
Raunak Daga <rdaga@college.harvard.edu>
2022-11-30 21:16:11 -0500

    Contact permissions

--- Pong/Features/Referrals/Views/ReferralsInviteFriendsView.swift
+++ Pong/Features/Referrals/Views/ReferralsInviteFriendsView.swift
@@ -1,5 +1,7 @@
 import SwiftUI
 import Foundation
+import Contacts
+import UIKit
 
 struct ReferralsInviteFriendsView: View {
     @ObservedObject var dataManager = DataManager.shared
@@ -38,56 +40,91 @@ struct ReferralsInviteFriendsView: View {
             
             // , id: \.id) { $contact in
 
-            Section {
-                // filter out contacts that don't appear in searchText
-                ForEach($contactsVM.contacts, id: \.id) {
-                    $contact in
-                    if (contact.phone != nil) {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text("\(contact.firstName) \(contact.lastName)")
-                                    .font(.headline)
-                                    .fontWeight(.semibold)
-                                Text("\(format(with: "(XXX) XXX-XXXX", phone: "\(contact.getLastTenDigits())"))")
-                                    .font(.footnote)
-                            }
-                            
-                            Spacer()
-                            
-                            VStack {
+            if contactsLoading {
+                HStack {
+                    Spacer()
+                    Text("Please enable contact permissions in settings.")
+                        .listRowSeparator(.hidden)
+                    Spacer()
+                }
+                .listRowSeparator(.hidden)
+                
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+                        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+                    } label: {
+                        Text("Settings")
+                            .font(.callout)
+                            .fontWeight(.medium)
+                            .padding(.vertical, 5)
+                            .padding(.horizontal, 10)
+                            .foregroundColor(Color.white)
+                            .overlay(RoundedRectangle(cornerRadius: 15).stroke().foregroundColor(Color.pongAccent))
+                            .background(Color.pongAccent)
+                            .cornerRadius(15)
+                            .listRowSeparator(.hidden)
+                        
+                    }
+                    .listRowSeparator(.hidden)
+                    
+                    Spacer()
+                }
+                .listRowSeparator(.hidden)
+            } else {
+                Section {
+                    // filter out contacts that don't appear in searchText
+                    ForEach($contactsVM.contacts, id: \.id) {
+                        $contact in
+                        if (contact.phone != nil) {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text("\(contact.firstName) \(contact.lastName)")
+                                        .font(.headline)
+                                        .fontWeight(.semibold)
+                                    Text("\(format(with: "(XXX) XXX-XXXX", phone: "\(contact.getLastTenDigits())"))")
+                                        .font(.footnote)
+                                }
+                                
                                 Spacer()
                                 
-                                HStack {
-                                    Button {
-                                        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
-                                        contact.sheetToggled.toggle()
-                                    } label: {
-                                        Text("Invite")
-                                            .font(.headline)
-                                            .fontWeight(.medium)
-                                    }
-                                    .buttonStyle(PlainButtonStyle())
-                                    .sheet(isPresented: $contact.sheetToggled) {
-                                        let referralCode = DAKeychain.shared["referralCode"]!
-                                        let url = "https://www.pong.college/refer/ This new app just came out for BU use my referral code \(referralCode)"
-                                        MessageComposeSheet(recipients: [contact.phone!], body: url) { messageSent in
-                                            print("MessageComposeView with message sent? \(messageSent)")
+                                VStack {
+                                    Spacer()
+                                    
+                                    HStack {
+                                        Button {
+                                            UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+                                            contact.sheetToggled.toggle()
+                                        } label: {
+                                            Text("Invite")
+                                                .font(.headline)
+                                                .fontWeight(.medium)
+                                        }
+                                        .buttonStyle(PlainButtonStyle())
+                                        .sheet(isPresented: $contact.sheetToggled) {
+                                            let referralCode = DAKeychain.shared["referralCode"]!
+                                            let url = "https://www.pong.college/refer/ This new app just came out for BU use my referral code \(referralCode)"
+                                            MessageComposeSheet(recipients: [contact.phone!], body: url) { messageSent in
+                                                print("MessageComposeView with message sent? \(messageSent)")
+                                            }
                                         }
                                     }
+                                    .padding(.vertical, 8)
+                                    .padding(.horizontal, 25)
+                                    .foregroundColor(Color.white)
+                                    .overlay(RoundedRectangle(cornerRadius: 20).stroke().foregroundColor(Color.pongAccent))
+                                    .background(Color.pongAccent)
+                                    .cornerRadius(20)
+                                    
+                                    Spacer()
                                 }
-                                .padding(.vertical, 8)
-                                .padding(.horizontal, 25)
-                                .foregroundColor(Color.white)
-                                .overlay(RoundedRectangle(cornerRadius: 20).stroke().foregroundColor(Color.pongAccent))
-                                .background(Color.pongAccent)
-                                .cornerRadius(20)
                                 
-                                Spacer()
                             }
-                            
+                            .listRowBackground(Color.pongSystemBackground)
+                            .listRowSeparator(.hidden)
                         }
-                        .listRowBackground(Color.pongSystemBackground)
-                        .listRowSeparator(.hidden)
                     }
                 }
             }
@@ -103,9 +140,8 @@ struct ReferralsInviteFriendsView: View {
         .onAppear {
             contactsVM.getContacts() {
                 success in
-                contactsLoading.toggle()
+                contactsLoading = false
             }
-            
         }
     }
     
@@ -116,7 +152,7 @@ struct ReferralsInviteFriendsView: View {
                 .font(Font.system(size: 16))
         }
         .padding(7)
-        .background(Color(hex: "F7F7F7"))
+        .background(Color.pongSearchBar)
         .cornerRadius(10)
     }
 }
